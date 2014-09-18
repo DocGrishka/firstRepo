6f99f5682946fc8027e1334609ca54b1978053e3
andreykaone <andreyka.one.two@gmail.com>
2014-09-18 13:05:08 +0400

    first commit

---
+++ 5_lab01/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+using Microsoft.Win32;
+
+namespace _5_lab01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void tbN_Click(object sender, RoutedEventArgs e)
+        {
+            ArrayList myAL = new ArrayList();
+            int index;
+            int itemCount = 5;
+            Random rnd1 = new Random();
+            int number;
+            lbMain.Items.Clear();
+            for (index = 1; index <= itemCount; index++)
+            {
+                number = -100 + rnd1.Next(200);
+                myAL.Add(number);
+                lbMain.Items.Add(number);
+            }
+        }
+
+        private void tbN2_Click(object sender, RoutedEventArgs e)
+        {
+            ArrayList myAL = new ArrayList();
+            int index;
+            int itemCount = 5;//Convert.ToInt32(tbN.Text)
+            Random rnd1 = new Random();
+            int number;
+            lbMain.Items.Clear();
+            lbMain.Items.Add("Исходный массив");
+            for (index = 1; index <= itemCount; index++)
+            {
+                number = -100 + rnd1.Next(200);
+                myAL.Add(number);
+                lbMain.Items.Add(number);
+            }
+            myAL.Sort();
+            lbMain.Items.Add("Отсортированный массив");
+            foreach (int elem in myAL)
+            {
+                lbMain.Items.Add(elem);
+            }
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog myDialog = new SaveFileDialog();
+            myDialog.Filter = "Текст(*.TXT)|*.TXT" + "|Все файлы (*.*)|*.* ";
+
+            if (myDialog.ShowDialog() == true)
+            {
+                string filename = myDialog.FileName;
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(filename, false))
+                {
+                    foreach (Object item in lbMain.Items)
+                    {
+                        file.WriteLine(item);
+                    }
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window win = new tasks();
+            win.Show();
+        }
+    }
+}
---
+++ 5_lab01/tasks.xaml.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.IO;
+using System.Text;
+
+
+namespace _5_lab01
+{
+    /// <summary>
+    /// Interaction logic for tasks.xaml
+    /// </summary>
+    public partial class tasks : Window
+    {
+        public tasks()
+        {
+            InitializeComponent();
+        }
+
+        private void firstTask_Click(object sender, RoutedEventArgs e)
+        {
+            //получаем адресс файла
+            string  filename       =   getFileName("");
+            int     numberOfLines  =   getNumberOfLines( 0, filename),
+                    count          =   0;
+
+
+            // забиваем массив
+            int[] mass = getMass(numberOfLines, filename, 0);
+
+
+            //считаем количество элементов массива, удовлетворяющих условию
+            for (int i = 1; i < numberOfLines - 1; i++)
+                if (mass[i] > mass[i - 1] && mass[i] > mass[i + 1])
+                    count++;
+
+            lbOne.Items.Add("Количество элементов масива, больших своих соседей: " + count + "\n");
+        }
+
+        private void secondTask_Click(object sender, RoutedEventArgs e)
+        {
+            string  filename        =   getFileName("");
+            int numberOfLines       =   getNumberOfLines(0, filename);
+
+            // забиваем массив
+            int[] mass = getMass(numberOfLines, filename, 0);
+
+            for (int i = 0; i < numberOfLines - 1; i++)
+                if(mass[i] > 25)
+                {
+                    lbOne.Items.Add("Номер первого элемента массива, большего 25: " + (i+1) + "\n");
+                    break;
+                }
+                    
+
+        }
+
+        private void thirdTask_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = getFileName("");
+            int numberOfLines = getNumberOfLines(0, filename),
+                summ = 0;
+
+            // забиваем массив
+            int[] mass = getMass(numberOfLines, filename, 0);
+
+            for (int i = 0; i < numberOfLines; i++)
+                if (mass[i] > mass[1])
+                  
+                    summ += mass[i];
+
+            lbOne.Items.Add("Сумма элеметов, больших 2ого элемента массива: " + summ + "\n");
+
+        }
+
+
+
+        public string getFileName(string filename)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+                filename = dlg.FileName;
+
+            return filename;
+        }
+
+        public int getNumberOfLines(int numberOfLines, string filename)
+        {
+            var reader = File.OpenText(filename);
+            while (reader.ReadLine() != null)
+                numberOfLines++;
+
+
+            return numberOfLines;
+
+        }
+
+        public int[] getMass(int numberOfLines, string filename, int index)
+        {
+            int[] mass = new int[numberOfLines];
+
+            foreach (string number in File.ReadLines(filename))
+            {
+                mass[index] = int.Parse(number);
+                index++;
+            }
+
+            return mass;
+        }
+    }
+}
